03a33e1931fc5fa97f49bb6bdfd81e62ea4eae4a
LeandroBelge <leandrobelge@gmail.com>
2017-07-18 15:41:11 -0300

    Linq to entities - Linq paralelo parte 2 (Utilizando a consulta paralelizada eu consigo utilizar o método ForAll() para otimizar o paralelismo)

--- AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/Program.cs
@@ -37,7 +37,8 @@ namespace AluraTunes
                                   select f).ToList();
 
                 Stopwatch stopwatch = Stopwatch.StartNew();
-                var queryCodigos = listaFaixas.AsParallel()
+
+                var queryCodigos = listaFaixas.AsParallel()//Parelelizando a consulta para otimizar o tempo de consulta.
                     .Select(f => new { 
                     Arquivo = string.Format("{0}\\{1}.jpg", IMAGENS, f.FaixaId),
                     Imagem = barcodeWriter.Write(string.Format("aluratunes.com/faixa/{0}", f.FaixaId))
@@ -45,13 +46,17 @@ namespace AluraTunes
 
                 var contagem = queryCodigos.Count();
                 stopwatch.Stop();
+
                 Console.WriteLine("Códigos gerados: {0} em {1} segundos", contagem, stopwatch.ElapsedMilliseconds/1000.0);
 
-                foreach (var item in queryCodigos)
-                {
-                    item.Imagem.Save(item.Arquivo, ImageFormat.Jpeg);    
-                }
+                stopwatch = Stopwatch.StartNew();
+                
+                queryCodigos.ForAll(item => item.Imagem.Save(item.Arquivo, ImageFormat.Jpeg));//Paralelizando a persistencia do código em arquivo. Isso otimiza o tempo de execução da aplicação.
+
+                stopwatch.Stop();
+                Console.WriteLine("Códigos salvos: {0} em {1} segundos", contagem, stopwatch.ElapsedMilliseconds / 1000.0);
             }
+            Console.ReadKey();
         }
     }
 }
