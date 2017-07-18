651937e4f8d2b50146eb4e4029ebc93de6369d8e
LeandroBelge <leandrobelge@gmail.com>
2017-07-18 14:36:06 -0300

    Linq to entities - Linq paralelo (Utilizando ZXing.net para geração de QRCode)

--- AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/Program.cs
@@ -2,40 +2,56 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using ZXing;
 
 namespace AluraTunes
 {
     class Program
     {
+        //Como gerar códigos QRCode utilizando ZXing.Net
+        private const string IMAGENS = "Imagens";
         static void Main(string[] args)
         {
+            var barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            barcodeWriter.Options = new ZXing.Common.EncodingOptions
+            {
+                Width = 100,
+                Height = 100
+            };
+            if (!Directory.Exists(IMAGENS))
+            {
+                Directory.CreateDirectory(IMAGENS);
+            }
             using (var contexto = new AluraTunesEntities())
             {
-                var mesAniversario = 1;
-                while (mesAniversario <= 12)
-                {
-                    
-                    Console.WriteLine("Mês: {0}", mesAniversario);
+                var listaFaixas = (from f in contexto.Faixas
+                                  select f).ToList();
 
-                    var query = (from f in contexto.Funcionarios
-                                where f.DataNascimento.Value.Month == mesAniversario
-                                orderby f.DataNascimento.Value.Month, f.DataNascimento.Value.Day 
-                                select f).ToList();//O método ToList() já compila a consulta, com isso a variável já guarda a resposta da consulta. 
-                                                   //Sem o ToList() a consulta só seria realizada quando o foreach iniciasse. Então a consulta com o ToList() é de execução imediata. 
-                    
-                    foreach (var f in query)
-                    {
-                        Console.WriteLine("{0:dd/MM}\t{1} {2}", f.DataNascimento, f.PrimeiroNome, f.Sobrenome);
-                    }
-                    mesAniversario++;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                var queryCodigos = listaFaixas.AsParallel()
+                    .Select(f => new { 
+                    Arquivo = string.Format("{0}\\{1}.jpg", IMAGENS, f.FaixaId),
+                    Imagem = barcodeWriter.Write(string.Format("aluratunes.com/faixa/{0}", f.FaixaId))
+                });
+
+                var contagem = queryCodigos.Count();
+                stopwatch.Stop();
+                Console.WriteLine("Códigos gerados: {0} em {1} segundos", contagem, stopwatch.ElapsedMilliseconds/1000.0);
+
+                foreach (var item in queryCodigos)
+                {
+                    item.Imagem.Save(item.Arquivo, ImageFormat.Jpeg);    
                 }
             }
-            Console.ReadKey();
         }
     }
 }
