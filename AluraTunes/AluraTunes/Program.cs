1b8406ae035747c5c3e5e3b47760901effa09a0b
LeandroBelge <leandrobelge@gmail.com>
2017-07-19 12:13:59 -0300

    Linq to entities stores procedure

--- AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/Program.cs
@@ -16,46 +16,31 @@ namespace AluraTunes
 {
     class Program
     {
-        //Como gerar códigos QRCode utilizando ZXing.Net
-        private const string IMAGENS = "Imagens";
         static void Main(string[] args)
         {
-            var barcodeWriter = new BarcodeWriter();
-            barcodeWriter.Format = BarcodeFormat.QR_CODE;
-            barcodeWriter.Options = new ZXing.Common.EncodingOptions
-            {
-                Width = 100,
-                Height = 100
-            };
-            if (!Directory.Exists(IMAGENS))
-            {
-                Directory.CreateDirectory(IMAGENS);
-            }
+            var clienteId = 17;
             using (var contexto = new AluraTunesEntities())
             {
-                var listaFaixas = (from f in contexto.Faixas
-                                  select f).ToList();
+                var query = from v in contexto.ps_Itens_Por_Cliente(clienteId)//Utilizando StoredProcedure como fonte de dados
+                            group v by new//Agrupando por campos anônimos
+                            {
+                                v.DataNotaFiscal.Year,
+                                v.DataNotaFiscal.Month
+                            }
+                                into Agrupado//Adicionando o valor do agrupamentoa uma variável
+                                orderby Agrupado.Key.Year, Agrupado.Key.Month//Ordenando o agrupamento
+                                select new//Criando objeto anônimo com o resultado esperado.
+                                {
+                                    Mes = Agrupado.Key.Month,
+                                    Ano = Agrupado.Key.Year,
+                                    Total = Agrupado.Sum(x => x.Total)
 
-                Stopwatch stopwatch = Stopwatch.StartNew();
-
-                var queryCodigos = listaFaixas.AsParallel()//Parelelizando a consulta para otimizar o tempo de consulta.
-                    .Select(f => new { 
-                    Arquivo = string.Format("{0}\\{1}.jpg", IMAGENS, f.FaixaId),
-                    Imagem = barcodeWriter.Write(string.Format("aluratunes.com/faixa/{0}", f.FaixaId))
-                });
-
-                var contagem = queryCodigos.Count();
-                stopwatch.Stop();
-
-                Console.WriteLine("Códigos gerados: {0} em {1} segundos", contagem, stopwatch.ElapsedMilliseconds/1000.0);
-
-                stopwatch = Stopwatch.StartNew();
-                
-                queryCodigos.ForAll(item => item.Imagem.Save(item.Arquivo, ImageFormat.Jpeg));//Paralelizando a persistencia do código em arquivo. Isso otimiza o tempo de execução da aplicação.
-
-                stopwatch.Stop();
-                Console.WriteLine("Códigos salvos: {0} em {1} segundos", contagem, stopwatch.ElapsedMilliseconds / 1000.0);
-            }
+                                };
+                foreach (var item in query)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}", item.Ano, item.Mes, item.Total);
+                }
+            }      
             Console.ReadKey();
         }
     }
